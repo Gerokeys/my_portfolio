3ee675fd752ea0a001e4f3e8cd161aa6c6dbc7b0
Gerokeys <shikukugerald111@gmail.com>
2024-08-29 11:49:52 +0300

    light mode local storage cemented

--- src/utils/dark-mode.js
+++ src/utils/dark-mode.js
@@ -1,11 +1,29 @@
 const darkMode = () => {
   const themeToggleBtn = document.querySelectorAll("#theme-toggle");
 
-  themeToggleBtn.forEach((btn) => {
-    btn.addEventListener("click", () => {
-      document.body.classList.toggle("light-mode");
-    });
-  });
+  // State
+  const theme = localStorage.getItem("theme");
+
+  // On mount
+  if (theme) {
+    document.body.classList.add(theme);
+  }
+
+  // Handlers
+  const handleThemeToggle = () => {
+    document.body.classList.toggle("light-mode");
+    if (document.body.classList.contains("light-mode")) {
+      localStorage.setItem("theme", "light-mode");
+    } else {
+      localStorage.removeItem("theme");
+      document.body.removeAttribute('class')
+    }
+  };
+
+  // Events
+  themeToggleBtn.forEach(
+    (btn) => btn.addEventListener("click", handleThemeToggle) // Corrected this line
+  );
 };
 
 export default darkMode;
